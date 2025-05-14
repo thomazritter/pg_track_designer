5f5a7334601359c46bfe044225c1b9f3f48c91d0
thomazrit <thomaz@usestable.com>
2025-05-14 14:22:47 -0300

    Initial Commit

---
+++ pg_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pg_project
+//
+//  Created by Thomaz Ritter on 14/05/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
